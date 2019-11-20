0e1e77b635143681b060e1b851094a2724546cf5
FiguerasMaxime <maxime.figueras@gmail.com>
2019-11-20 12:45:36 +0100

    Third commit : Visuel des touches refait avec les règles, l'affichage des scores comptabilisés suivant l'ordre des touches

--- js/script.js
+++ js/script.js
@@ -6,10 +6,14 @@ var overlay = document.getElementById("overlay");
 var quit = document.getElementById("quit");
 var play = document.getElementById("play");
 
+var arraySuite = []; // suite de couleurs
+var arraySaisi = [];
+
 const redDiv = document.getElementById("red");
 const greenDiv = document.getElementById("green");
 const blueDiv = document.getElementById("blue");
 const yellowDiv = document.getElementById("yellow");
+
 
 // apparition et disparition du #overlay
 if(infos.addEventListener("click", function() {
@@ -27,57 +31,60 @@ else {
 
 function Start() {
     var n = 0; //nombres de touches appuyés qui vont servir à l'incrémentation
-    var nbColorPerRound = 8; //nombres maximum de couleurs qui défile sur un tour
-    var arraySuite = []; // suite de couleurs
+    var nbColorPerRound = 5; //nombres maximum de couleurs qui défile sur un tour
 
     function ColorRandom() {
         while(n < nbColorPerRound) {
+            n++; //passe au prochain tirage
             var arrayColors = ["red", "green", "blue", "yellow"]; // toutes les couleurs super simon
             var colorChoosen = arrayColors[Math.floor(Math.random() * arrayColors.length)]; // PAS OUBLIE PARCE = CEST DE LA CHIASSE, on récupère l'entier avec floor dans le tableau colors avec *
             arraySuite.push(colorChoosen); // cela ressort une couleur
-            n++; //passe au prochain tirage
         }
     };
 
     function greenColor() {
-        var green = document.getElementById("green");
         setTimeout(() => {
-            green.style.backgroundColor = "green";
-
-        }, 200);
-        green.style.backgroundColor = "lightblue"; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+            green.style.transform = 'scale(1)';
+            green.style.opacity = "1";
+        }, 300);
+        green.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        green.style.opacity = "0.6";
     };
 
     function redColor() {
-        var red = document.getElementById("red");
         setTimeout(() => {
-            red.style.backgroundColor = "red";
-        }, 200);
-        red.style.backgroundColor = "lightblue"; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+            red.style.transform = 'scale(1)';
+            red.style.opacity = "1";
+        }, 300);
+        red.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        red.style.opacity = "0.6";
     };
 
     function blueColor() {
-        var blue = document.getElementById("blue");
         setTimeout(() => {
-            blue.style.backgroundColor = "blue";
-        }, 200);
-        blue.style.backgroundColor = "lightblue"; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+            blue.style.transform = 'scale(1)';
+            blue.style.opacity = "1";
+        }, 300);
+        blue.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        blue.style.opacity = "0.6";
     };
     
     function yellowColor() {
-        var yellow = document.getElementById("yellow");
         setTimeout(() => {
-            yellow.style.backgroundColor = "yellow";
-        }, 200);
-        yellow.style.backgroundColor = "lightblue"; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+            yellow.style.transform = 'scale(1)';
+            yellow.style.opacity = "1";
+        }, 300);
+        yellow.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        yellow.style.opacity = "0.6";
     };
 
     function Round() {
+        console.log("Choix des couleurs")
         for (let i = 0; i < nbColorPerRound; i++) { // fait autant de boucles que de nombres de couleurs par tour
             setTimeout(() => {
-                switch(arraySuite[i]){ // la couleur choisie fait partie d'un seul case, ensuite elle lui change la couleur avec la fonction en question
+                switch(arraySuite[i]){ // la couleur choisie fait partie d'une seule case, ensuite elle lui change la couleur avec la fonction en question
                     case 'red':
-                        redColor(); //
+                        redColor(); 
                         console.log("red");
                         break;
                     case 'green':
@@ -104,28 +111,94 @@ function Start() {
     ColorRandom();
     SelectColor();
 
+    setTimeout(function() { // Permet de finir le tour
+        console.log("A VOUS");
+    }, 1000 * nbColorPerRound);
+
 };
 
 play.addEventListener("click", function() {
     Start(); // lancement du jeu lorsqu'on appuie sur le #play
 });
 
+
+var i = 0;
+
+function increase()
+{
+    i++;
+    document.getElementById('nbTouches').innerHTML= +i;
+}
+
+
 function SelectColor() { //choix de la couleur suite à la touche cliquée
+
+    nbTouches = 0;
+
     redDiv.addEventListener("click", function() {
         console.log("redDiv");
+        setTimeout(() => {
+            red.style.transform = 'scale(1)';
+            red.style.opacity = "1";
+        }, 300);
+        red.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        red.style.opacity = "0.6";
+        increase();
+        arraySaisi.push("red");
+        console.log(arraySaisi);
+        ColorConfirmed();
     });
 
     greenDiv.addEventListener("click", function() {
         console.log("greenDiv");
+        setTimeout(() => {
+            green.style.transform = 'scale(1)';
+            green.style.opacity = "1";
+        }, 300);
+        green.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        green.style.opacity = "0.6";
+        increase();
+        arraySaisi.push("green");
+        console.log(arraySaisi);
+        ColorConfirmed();
     });
 
     blueDiv.addEventListener("click", function() {
         console.log("blueDiv");
+        setTimeout(() => {
+            blue.style.transform = 'scale(1)';
+            blue.style.opacity = "1";
+        }, 300);
+        blue.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        blue.style.opacity = "0.6";
+        increase();
+        arraySaisi.push("blue");
+        console.log(arraySaisi);
+        ColorConfirmed();
     });
 
     yellowDiv.addEventListener("click", function() {
         console.log("yellowDiv");
+        setTimeout(() => {
+            yellow.style.transform = 'scale(1)';
+            yellow.style.opacity = "1";
+        }, 300);
+        yellow.style.transform = 'scale(0.8)'; //mettre cela car sinon la div n'a plus de couleur si elle a été tiré au sort
+        yellow.style.opacity = "0.6";
+        increase();
+        arraySaisi.push("yellow");
+        console.log(arraySaisi);
+        ColorConfirmed();
     });
+
+    function ColorConfirmed() {
+        if(arraySaisi[0] === arraySuite[0]) {
+            console.log("Bonne couleur");
+        }
+        else {
+            console.log("Mauvaise couleur");
+        }
+    }
 
 };
 
